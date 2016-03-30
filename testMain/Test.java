2b1a708d25e279c12ac54027e1e7cc7d0ad8ecda
Evan <txtlu0@gmail.com>
2016-03-30 21:18:45 +0800

    Using ArrayList to store the three dicttree, learn to use generic

--- testMain/Test.java
+++ testMain/Test.java
@@ -1,11 +1,25 @@
 package testMain;
 import romaToInt.Solution;
+import romaToInt.DictTree;
+import romaToInt.Node;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
+	
+	public int parse(ArrayList<DictTree> dictTree, String str) {
+		int len = dictTree.size();
+		int res = 0;
+		for (DictTree dt : dictTree) {
+			res += dt.parseStr(str);
+			
+		}
+		return res;
+	}
+	
 
 	@org.junit.Test
 	public void test() {
@@ -14,10 +28,20 @@ public class Test {
 		Scanner in = new Scanner(System.in);
 		str = in.nextLine();
 		
-		Solution one = new Solution();
-		int num = 0;
-		num = one.romanToInt(str);
-		System.out.println(num);
+		// Solution one = new Solution();
+		// int num = 0;
+		// num = one.romanToInt(str);
+		// System.out.println(num);
+		DictTree C = Node.buildDT('C', 'D', 'M', 100);
+		DictTree X = Node.buildDT('X', 'L', 'C', 10);
+		DictTree I = Node.buildDT('I', 'V', 'X', 1);
+		ArrayList<DictTree> dictTree = new ArrayList<>();
+		dictTree.add(C);
+		dictTree.add(X);
+		dictTree.add(I);
+
+		int result = parse(dictTree, str);
+		System.out.println((result));
 		
 		
 	}
